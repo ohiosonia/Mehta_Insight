2dc43dcee30df87d646acc8a6701422457d013ca
smehta930 <smehta930@gmail.com>
2015-07-15 23:45:29 -0400

    Remove local reference from file

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,10 +1,12 @@
+# example of program that captures 'running' median of tweets
 import numpy as np
 
-tweets = open('/Users/soniamehta/Desktop/Mehta_Insight/tweet_input/tweets.txt')
-results = open('/Users/soniamehta/Desktop/Mehta_Insight/tweet_output/ft2.txt','w')
+tweets = open('/tweet_input/tweets.txt')
+results = open('/tweet_output/ft2.txt','w')
 
 length_tweet = []
 
+# Get all words
 def all_words(word_list):
     word_count = {}
     for word in word_list:
@@ -14,10 +16,11 @@ def all_words(word_list):
             word_count[word] = 1
     return len(word_count)
     
-
+# Get total words
 for line in iter(tweets):
      words =  line.split(' ',len(line)-1)
      length_tweet.append(all_words(words))
 
+# Print median
 for l in range(len(length_tweet)):
     results.write('%.3f\n'%np.median(length_tweet[0:l+1]))

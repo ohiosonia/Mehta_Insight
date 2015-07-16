b6b85a714af8672cda3ca87998f66d1adb39c73f
smehta930 <smehta930@gmail.com>
2015-07-15 21:55:49 -0400

    First push

--- src/median_unique.py
+++ src/median_unique.py
@@ -1,1 +1,24 @@
-# example of program that calculates the median number of unique words per tweet.
+import numpy as np
+
+def create_dictionary(word_list):
+    word_count = {}
+    for word in word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return len(word_count)
+    
+tweets = open('tweet_input/tweets.txt')
+text_file = open('tweet_output/ft2.txt','w')
+length_tweet = []
+
+for line in iter(tweets):
+     words =  line.split(' ',len(line)-1)
+     length_tweet.append(create_dictionary(words))
+
+for l in range(len(length_tweet)):
+    text_file.write('%.3f\n'%np.median(length_tweet[0:l+1]))
+
+tweets.close()
+text_file.close()
--- src/words_tweeted.py
+++ src/words_tweeted.py
@@ -1,1 +1,24 @@
 # example of program that calculates the total number of times each word has been tweeted.
+import csv
+import re
+import sys
+from collections import Counter
+
+tweets = ('/Users/soniamehta/Desktop/cc-example/tweet_input/tweets.txt')
+tweets = open(tweets)
+
+
+# Calculate word frequencies from input tweets.
+word_counts = {}
+for tweet in tweets: #sys.stdin:
+    words = re.split('\s+', tweet)    
+    word_counts.update(Counter(words))
+
+# Print word frequencies.
+for key in sorted(word_counts.keys()):  # iterate through sorted dictionary
+    a =  [key, word_counts[key]]
+    print a
+
+with open("output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(a)
